6dea4662a1e0df50919df3279020c2e13c6302c4
SSkuratov <SSkuratov@it-one.ru>
2024-04-17 22:56:20 +0300

    Initial commit

---
+++ src/main/java/com/modelstore/modelstore/ModelStoreApplication.java
@@ -0,0 +1,13 @@
+package com.modelstore.modelstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModelStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/modelstore/modelstore/ModelStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modelstore.modelstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
